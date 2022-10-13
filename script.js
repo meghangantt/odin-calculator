a2a22465da9280d6f507a94bdba262ffe9b7a598
meghangantt <meghangantt@gmail.com>
2022-10-13 14:48:31 -0400

    Add functionality to operation buttons

--- script.js
+++ script.js
@@ -7,32 +7,53 @@ const multiply = (x, y) => x*y;
 const divide = (x, y) => x/y;
 
 const operate = (operator, x, y) => {
-    let result;
+    let action;
     if (operator==="+") {
-        result = add(x, y);
+        action = add(x, y);
     } else if (operator==="-") {
-       result = subtract(x, y);
-    } else if (operator==="*") {
-        result = multiply(x, y);
-    } else if (operator==="/") {
-        result =divide(x, y);
+        action = subtract(x, y);
+    } else if (operator==="×") {
+        action = multiply(x, y);
+    } else if (operator==="÷") {
+        action = divide(x, y);
     }
-    return result;
+    return action;
 };
 
-const theDisplay = document.querySelector('.display');
+let theDisplay = document.querySelector('.display');
 
 const numbers = document.querySelectorAll('.number');
 
-let lastDigit;
 let currentValue;
+let prevValue;
+let operator;
+let needsClear = false;
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
-        lastDigit = number.textContent;
-        theDisplay.textContent += lastDigit;
+        if (needsClear) {
+            theDisplay.textContent = undefined;
+            needsClear = false;
+        };
+        theDisplay.textContent += number.textContent;
         currentValue = +(theDisplay.textContent);
     });
 });
 
+const operations = document.querySelectorAll('.operator');
 
+operations.forEach((operation) => {
+    operation.addEventListener('click', () => {
+        operator = operation.textContent;
+        prevValue = currentValue;
+        currentValue = undefined;
+        needsClear = true;
+    });
+});
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener('click', () => {
+    theDisplay.textContent = operate(operator, prevValue, currentValue);
+    needsClear = true;
+});
